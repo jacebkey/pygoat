2e991c47693df9f86ccf7ffcdf7244e2fe08b803
jace.key <jacebkey@gmail.com>
2021-12-07 00:43:50 -0600

    test

--- pygoat/introduction/tests.py
+++ pygoat/introduction/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+# Create your tests here.
+class DummyTest(TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(True, True)
